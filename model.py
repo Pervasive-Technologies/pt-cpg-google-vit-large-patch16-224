c21e34ac8cce80c18dc97dc83cacf92db460a93a
Rodolfo Lomascolo <r.lomascolo@rodolfolomascolo.com>
2025-02-10 17:02:35 +0100

    Add files via upload

--- model.py
+++ model.py
@@ -4,19 +4,46 @@ from torchvision import transforms
 from PIL import Image
 import numpy as np
 import os
-from google.cloud import storage
-from google.oauth2 import service_account
 import requests
 import json
+import logging
+import os
 
 # Example usage
 
-MODEL_PATH = "model.pt"
+MODEL_PATH = "./model.pt"
+MODEL_URL = "https://storage.googleapis.com/fiftyone-models/pt-cpg-google-vit-large-patch16-224/model.pt"  # Replace with your actual URL
+MODEL_NAME = "pt-cpg/google/vit-large-patch16-224"
+
+def download_model():
+    """Downloads the model.
+
+    Args:
+        model_name: the name of the model to download, as declared by the
+            ``base_name`` and optional ``version`` fields of the manifest
+        model_path: the absolute filename or directory to which to download the
+            model, as declared by the ``base_filename`` field of the manifest
+    """
+    
+    """
+    Downloads model.pt if it does not exist locally.
+    """
+    if not os.path.exists(MODEL_PATH):
+        print(f"Downloading model from {MODEL_URL}...")
+        response = requests.get(MODEL_URL, stream=True)
+        with open(MODEL_PATH, "wb") as f:
+            for chunk in response.iter_content(chunk_size=1024):
+                if chunk:
+                    f.write(chunk)
+        print("Download complete.")
+
 
 class ViTEmbeddingModel:
     def __init__(self):
         self.device = "cuda" if torch.cuda.is_available() else "cpu"        
         # Load the model architecture (ViT-Large Patch16-224)
+        download_model()
+        
         self.model = timm.create_model("vit_large_patch16_224", pretrained=False, num_classes=0)  # No classifier head
         self.model.load_state_dict(torch.load(MODEL_PATH, map_location=self.device))
         self.model.to(self.device).eval()
