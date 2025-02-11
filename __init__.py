11fb5bc01afcaf17feb3247f191f99d2f2a3d272
Rodolfo Lomascolo <r.lomascolo@rodolfolomascolo.com>
2025-02-11 18:49:44 +0100

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -21,8 +21,8 @@ import fiftyone.core.models as fom
 # Example usage
 
 MODEL_PATH = "./"
-MODEL_FILENAME = "./model.pt"
-MODEL_URL = "https://storage.googleapis.com/fiftyone-models/pt-cpg-google-vit-large-patch16-224/model.pt"  # Replace with your actual URL
+MODEL_FILENAME = "./model.safetensors"
+MODEL_URL = "https://storage.googleapis.com/fiftyone-models/pt-cpg-google-vit-large-patch16-224/model.safetensors"  # Replace with your actual URL
 MODEL_NAME = "pt-cpg-google-vit-large-patch16-224"
 import os
 
